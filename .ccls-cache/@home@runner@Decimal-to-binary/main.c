6d5b4ce151dbd7876be9da789703e920dcfe652d
64070501041 Panumeth Khongsawatkiat <65102979+Kobayashi-UwU@users.noreply.github.com>
2023-02-01 04:40:51 +0000

    feat: initial commit

--- .ccls-cache/@home@runner@Decimal-to-binary/main.c
+++ .ccls-cache/@home@runner@Decimal-to-binary/main.c
@@ -1,11 +1,119 @@
 #include <stdio.h>
+#define BIT 8
 
-//sign-bit
+void signBit(int input) {
+  int output[BIT];
+  int count = 0, sign = 0;
 
-//1's complement
+  if (input > 127 || input < -127) {
+    printf("Overflow");
+    return;
+  }
 
-//2's complement
+  if (input < 0) {
+    sign = 1;
+    input *= -1;
+  }
 
-int main(void) {
+  while (input > 0) {
+    output[count++] = input % 2;
+    input /= 2;
+  }
+
+  printf("%d", sign);
+  for (int i = BIT - 2; i >= count; i--) {
+    printf("0");
+  }
+
+  for (int i = count - 1; i >= 0; i--) {
+    printf("%d", output[i]);
+  }
+}
+
+void firstComplement(int input) {
+  int output[BIT];
+  int count = 0;
+  int sign = 0;
+
+  if (input > 127 || input < -127) {
+    printf("Overflow");
+    return;
+  }
+
+  for (int i = 0; i < BIT; i++) {
+    output[i] = 0;
+  }
+
+  if (input < 0) {
+    input *= -1;
+    sign = 1;
+  }
+
+  while (input > 0) {
+    output[count++] = input % 2;
+    input /= 2;
+  }
+
+  if (sign) {
+    for (int i = 0; i < BIT; i++) {
+      output[i] = !output[i];
+    }
+  }
+
+  for (int i = BIT - 1; i >= 0; i--) {
+    printf("%d", output[i]);
+  }
+}
+
+void secondComplement(int input) {
+  int output[BIT];
+  int count = 0;
+  int sign = 0;
+
+  if (input > 127 || input < -128) {
+    printf("Overflow");
+    return;
+  }
+
+  for (int i = 0; i < BIT; i++) {
+    output[i] = 0;
+  }
+
+  if (input < 0) {
+    input *= -1;
+    input -= 1;
+    sign = 1;
+  }
+
+  while (input > 0) {
+    output[count++] = input % 2;
+    input /= 2;
+  }
+
+  if (sign) {
+    for (int i = 0; i < BIT; i++) {
+      output[i] = !output[i];
+    }
+  }
+
+  for (int i = BIT - 1; i >= 0; i--) {
+    printf("%d", output[i]);
+  }
+}
+
+int main() {
+  int num;
+  printf("input : ");
+  scanf("%d", &num);
   
+  printf("\nSign and magnitude : ");
+  signBit(num);
+  
+  printf("\n1's complement : ");
+  firstComplement(num);
+  
+  printf("\n2's complement : ");
+  secondComplement(num);
+  
+  return 0;
 }
